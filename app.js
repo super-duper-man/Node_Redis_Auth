e40ccf99420dfed367bdbb0c5c9a92644a9f6c5d
super-duper-man <m.r.fouladi7@gmail.com>
2023-12-22 18:42:59 +0330

    initial redis connection

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
+const {createClient} = require('redis')
+const RedisStore = require('connect-redis').default
+const redisClient = createClient({legacyMode: true})
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
+
+redisClient.connect().catch(error => console.log(`Could not connect to redis`, error))
 
 const app = express();
 
@@ -17,9 +22,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    resave: false,
-    saveUninitialized: true,
+    store: new RedisStore({
+      client: redisClient,
+  }),
     secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      maxAge: 1000 * 60 * 10
+    }
   })
 );
 
